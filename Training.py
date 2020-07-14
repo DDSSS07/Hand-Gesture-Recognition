56c9c64ff0da7c0c4ffd5f6a4a7ea0afba137b4a
soundaryasenthil02 <soundaryabarani@gmail.com>
2020-07-14 10:39:12 +0530

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,3 +1,4 @@
+#import lib
 import tensorflow as tf
 import tflearn
 from tflearn.layers.conv import conv_2d,max_pool_2d
@@ -6,21 +7,22 @@ from tflearn.layers.estimator import regression
 import cv2
 from sklearn.utils import shuffle
 
+#Loading Datas
 #Swing Images
 load_images = []
-for img in range(0, 1000):
+for img in range(0, 800):
     image = cv2.imread('Dataset/SwingImages/swing_' + str(img) + '.png')
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     load_images.append(gray_img.reshape(89, 100, 1))
 
 #Palm images
-for img in range(0, 1000):
+for img in range(0, 800):
     image = cv2.imread('Dataset/PalmImages/palm_' + str(img) + '.png')
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     load_images.append(gray_img.reshape(89, 100, 1))
     
 #Fist images
-for img in range(0, 1000):
+for img in range(0, 800):
     image = cv2.imread('Dataset/FistImages/fist_' + str(img) + '.png')
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     load_images.append(gray_img.reshape(89, 100, 1))
@@ -29,17 +31,18 @@ for img in range(0, 1000):
 #Define Output vector 
 
 vectors = []
-for img in range(0, 1000):
+for img in range(0, 800):
     vectors.append([1, 0, 0])
 
-for img in range(0, 1000):
+for img in range(0, 800):
     vectors.append([0, 1, 0])
 
-for img in range(0, 1000):
+for img in range(0, 800):
     vectors.append([0, 0, 1])
 
 test_images = []
 
+#Preprocessing the data
 #Swing images
 for img in range(0, 100):
     image = cv2.imread('Dataset/SwingTest/swing_' + str(img) + '.png')
@@ -72,8 +75,9 @@ for img in range(0, 100):
 # Creating CNN Model
 tf.reset_default_graph()
 
-# Convolution Layer
 network=input_data(shape=[None,89,100,1],name='input')
+# Convolution Layer
+# Conv2D wrapper, with bias and relu activation
 network=conv_2d(network,32,2,activation='relu')
 
 # Max Pooling (down-sampling)
@@ -110,14 +114,18 @@ network=dropout(network,0.75)
 # Fully connected layer
 network=fully_connected(network,3,activation='softmax')
 
+# With TFLearn estimators
+# Define loss and optimizer
 network=regression(network,optimizer='adam',learning_rate=0.001,loss='categorical_crossentropy',name='regression')
 
+# Train using Classifier
 model=tflearn.DNN(network,tensorboard_verbose=0)
 
 # Shuffle Training Data
 load_images, vectors = shuffle(load_images, vectors, random_state=0)
 
 # Perform the model training
+# Training the model
 model.fit(load_images, vectors, n_epoch=50, validation_set = (test_images, labels), snapshot_step=100, show_metric=True, run_id='convnet_coursera')
 
 model.save("Trained_Model/GestureRecogModel.tfl")
